7bcaa1154c8fde974951a63ae672a6fb992d43ee
darshanvarasani <darshanvarasani789@gmail.com>
2021-06-11 19:11:14 +0530

    done

---
+++ venv1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!G:\python\Django-Library-Management-System-master\Django-Library-Management-System-master\venv1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
